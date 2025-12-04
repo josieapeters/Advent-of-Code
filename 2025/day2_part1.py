0047ad102a83e087afc2d94a4c4b1bb7dae20410
Josie <josieapeters@gmail.com>
2025-12-04 18:47:10 +0000

    Day 2 solutions

---
+++ 2025/day2_part1.py
@@ -0,0 +1,34 @@
+"""
+Advent of Code 2025 - Day 2 part i
+"""
+
+import sys
+import csv
+
+
+def find_fake_ids(id_range: str) -> list[int]:
+    lower, upper = [int(x) for x in id_range.split('-')]
+    fake_ids = []
+    for id in range(lower, upper+1):
+        half_id_length = int(len(str(id))/2)
+        if str(id) == (str(id)[0:half_id_length] * 2):
+            fake_ids.append(id)
+    return fake_ids
+
+
+if __name__ == '__main__':
+    input_file = str(
+        sys.argv[1]
+        if len(sys.argv) > 1
+        else FileNotFoundError("Input file not specified")
+    )
+    with open(input_file) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            ranges = row
+        fake_ids = []
+        for id_range in ranges:
+            temp_ids = find_fake_ids(id_range)
+            for x in temp_ids:
+                fake_ids.append(x)
+        print(f'Total sum of invalid IDs is: {sum(fake_ids)}')
